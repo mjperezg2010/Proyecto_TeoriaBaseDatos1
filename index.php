3ee3d3fdbcd678c2ab9bf747d7bd23c6ad92c215
MartinPerez <mjperezg2010@hotmail.com>
2019-03-17 18:00:09 -0600

    El proyecto delos proyectos

---
+++ index.php
@@ -0,0 +1,136 @@
+<!doctype html>
+<html>
+<head>
+<meta charset="utf-8">
+ <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="css/bootstrap.css">
+    
+    <!-- Estilos personalizados -->
+	<link rel="stylesheet" href="css/estilos.css">
+<title>Documento sin título</title>
+</head>
+<body bgcolor="#3333CC" background="468252-peores-conversiones-recreativa.jpg">
+	<header>
+        	<h1 style="color:#FFF" align="center" class="card-header">Maquinas Recreativas Brayan Johnson </h1>
+    </header>
+	
+
+  <!-- <section class="main row">
+  		<div class="col-md-3">
+        </div>
+        <div class="col-md-6">
+        	
+            <br> <br>
+            
+                 <div id="carouselExampleControls" class="carousel slide" data-ride="carousel">
+                  <div class="carousel-inner">
+                    <div class="carousel-item active">
+                    <form action="Ensamblaje.php" method="post">
+                     	<input name="je" type="image" src="AAA.jpg">
+                     </form>
+                    </div>
+                    <div class="carousel-item">
+                      <form action="comercio_eleccion.php" method="post">
+                     	<input name="je" type="image" src="AAA.jpg">
+                     </form>
+                    </div>
+                    <div class="carousel-item">
+                      <img src="..." class="d-block w-100" alt="...">
+                    </div>
+                  </div>
+                  <a class="carousel-control-prev" href="#carouselExampleControls" role="button" data-slide="prev">
+                    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+                    <span class="sr-only">Previous</span>
+                  </a>
+                  <a class="carousel-control-next" href="#carouselExampleControls" role="button" data-slide="next">
+                    <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                    <span class="sr-only">Next</span>
+                  </a>
+                </div>
+               
+        	
+              
+        </div>
+        </section> -->
+        
+         <!-- Div de las columnas -->
+        
+       <!-- <div class="col-md-4">
+        	<h2>Creaciones</h2>
+            
+        </div>
+    -->    
+    		<br> <br>
+    	<section class="main row">
+        <div class="col-md-3">
+        </div>
+        
+        <div class="col-md-6">
+            <div class="bd-example">
+          <div id="carouselExampleCaptions" class="carousel slide" data-ride="carousel">
+            <ol class="carousel-indicators">
+              <li data-target="#carouselExampleCaptions" data-slide-to="0" class="active"></li>
+              <li data-target="#carouselExampleCaptions" data-slide-to="1"></li>
+              <li data-target="#carouselExampleCaptions" data-slide-to="2"></li>
+            </ol>
+            <div class="carousel-inner">
+              <div class="carousel-item active">
+                <form action="comercio_eleccion.php" method="post">
+                     	<input name="je" type="image" src="AA.jpg">
+                     </form>
+                <div class="carousel-caption d-none d-md-block">
+                  <h5>Etapa de Ensamblaje</h5>
+                  <p>En esta etapa se ensamblan maquinas recreativas.</p>
+                </div>
+              </div>
+              <div class="carousel-item">
+                <form action="comercio_eleccion.php" method="post">
+                     	<input name="je" type="image" src="Comercio.jpg">
+                     </form>
+                <div class="carousel-caption d-none d-md-block">
+                  <h5>Comercios </h5>
+                  <p>Aqui se distribuyen las maquinas a sus comercios respectivos</p>
+                </div>
+              </div>
+              <div class="carousel-item">
+                <form action="comprobacion y reparacion.php" method="post">
+                     	<input name="je" type="image" src="Comprobacio.jpg">
+                 </form>
+                <div class="carousel-caption d-none d-md-block">
+                  <h5>Comprobacion y Reparacion de Maquinas</h5>
+                  <p>Aqui se verifican las maquinas con errores en ensamblaje y maquinas dañadas en comercios</p>
+                </div>
+              </div>
+            </div>
+            <a class="carousel-control-prev" href="#carouselExampleCaptions" role="button" data-slide="prev">
+              <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+              <span class="sr-only">Previous</span>
+            </a>
+            <a class="carousel-control-next" href="#carouselExampleCaptions" role="button" data-slide="next">
+              <span class="carousel-control-next-icon" aria-hidden="true"></span>
+              <span class="sr-only">Next</span>
+            </a>
+          </div>
+        </div>
+        </div>
+            
+   </section>
+        
+        <div class="alert alert-primary" role="alert">
+          A simple primary alert—check it out!
+        </div>
+        
+        <div class="alert alert-success" role="alert">
+          A simple success alert—check it out!
+        </div>
+
+
+    <!-- Optional JavaScript -->
+    <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" 			crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity=	"sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
+    <script src="js/bootstrap.js"></script>
+</body>
+</html>
